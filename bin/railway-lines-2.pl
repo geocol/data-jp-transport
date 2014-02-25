7e4bd115ef4f482df1813f172470e22c66a7c928
Wakaba <wakabatan@hatena.ne.jp>
2014-02-25 16:38:44 +0900

    Added line properties

--- bin/railway-lines-2.pl
+++ bin/railway-lines-2.pl
@@ -69,4 +69,50 @@ delete $Data->{野岩鉄道会津鬼怒川線}->{company_wrefs}->{東武鉄道};
 ## <http://ja.wikipedia.org/wiki/JR%E6%9D%B1%E8%A5%BF%E7%B7%9A>
 $Data->{JR東西線}->{company_wrefs}->{関西高速鉄道} = 1;
 
+for my $line (keys %$Data) {
+    my $data = $Data->{$line};
+    if ($data->{company_wrefs}->{北海道旅客鉄道} or
+        $data->{company_wrefs}->{東日本旅客鉄道} or
+        $data->{company_wrefs}->{東海旅客鉄道} or
+        $data->{company_wrefs}->{西日本旅客鉄道} or
+        $data->{company_wrefs}->{四国旅客鉄道} or
+        $data->{company_wrefs}->{九州旅客鉄道} or
+        $data->{company_wrefs}->{日本貨物鉄道} or
+        $data->{company_wrefs}->{日本国有鉄道}) {
+        $data->{jr} = 1;
+    }
+
+    ## <http://ja.wikipedia.org/wiki/%E6%96%B0%E5%B9%B9%E7%B7%9A>
+    $data->{shinkansen} = 'full' if $line =~ /新幹線/;
+    $data->{shinkansen} = 'mini' if $line =~ /秋田新幹線|山形新幹線/;
+    $data->{shinkansen} = 'misc' if $line =~ /博多南線|ガーラ湯沢/;
+
+    ## <http://ja.wikipedia.org/wiki/%E3%82%B1%E3%83%BC%E3%83%96%E3%83%AB%E3%82%AB%E3%83%BC>
+    $data->{cablecar} = 1 if $line =~ /ケーブル|鋼索|竜飛斜坑線|比叡山鉄道/;
+
+    ## <http://ja.wikipedia.org/wiki/%E3%83%88%E3%83%AD%E3%83%AA%E3%83%BC%E3%83%90%E3%82%B9>
+    $data->{bus} = 'trolley' if $line =~ /無軌条|トロリー/;
+
+    ## <http://ja.wikipedia.org/wiki/%E3%82%AC%E3%82%A4%E3%83%89%E3%82%A6%E3%82%A7%E3%82%A4%E3%83%90%E3%82%B9>
+    $data->{bus} = 'guided' if $line =~ /ガイドウェイ/;
+
+    $data->{bus} = 'brt' if $line =~ /BRT/;
+
+    ## <http://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E8%B7%AF%E9%9D%A2%E9%9B%BB%E8%BB%8A%E4%B8%80%E8%A6%A7>
+    $data->{tram} = 1 if $line =~ /札幌市電|函館市企業局|都電荒川線|東田本線|富山市内軌道|阪堺線|上町線|東山本線|清輝橋線|広島電鉄|土佐電気鉄道|伊予鉄道|長崎電気軌道|熊本市電|鹿児島市電/;
+    $data->{tram} = 'partial' if $line =~ /東急世田谷線|ライトレール|万葉線|福武線|京津線|石山坂本線|嵐山本線|北野線/;
+    delete $data->{tram} if $line =~ /万葉線新湊港線/;
+
+    ## <http://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E5%9C%B0%E4%B8%8B%E9%89%84>
+    $data->{subway} = 1 if $line =~ /地下鉄|メトロ|みなとみらい/;
+    $data->{subway} = 'partial' if $line =~ /広島新交通1号線/;
+
+    ## <http://ja.wikipedia.org/wiki/%E6%96%B0%E4%BA%A4%E9%80%9A%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0>
+    $data->{agt} = 1 if $line =~ /埼玉新都市交通|東京臨海新交通臨海線|シーサイドライン|日暮里・舎人ライナー|西武山口線|ユーカリが丘線|南港ポートタウン線|神戸新交通|広島高速交通|桃花台新交通/;
+    $data->{hsst} = 1 if $line =~ /東部丘陵線/;
+
+    ## <http://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E3%83%A2%E3%83%8E%E3%83%AC%E3%83%BC%E3%83%AB>
+    $data->{monorail} = 1 if $line =~ /モノレール|上野懸垂線|ディズニーリゾートライン|大阪高速鉄道|広島短距離交通瀬野線|北九州高速鉄道/;
+}
+
 print perl2json_bytes_for_record $Data;
