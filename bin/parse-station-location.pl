559b694b93455dc225574d954529c9b6d940939d
Wakaba <wakabatan@hatena.ne.jp>
2014-02-25 11:55:30 +0900

    Improved handling of Hokkaidou addresses

--- bin/parse-station-location.pl
+++ bin/parse-station-location.pl
@@ -19,18 +19,6 @@ my $regions_f = $root_d->file ('local/regions.json');
 my $regions = file2perl $regions_f;
 $regions = {%$regions, map { %{$regions->{$_}->{areas} or {}} } keys %$regions};
 
-for my $area (keys %{$regions->{北海道}->{areas}}) {
-  if ($area =~ /^上川郡/) {
-    $regions->{北海道}->{areas}->{上川郡}->{areas}->{$_}
-        = $regions->{北海道}->{areas}->{$area}->{areas}->{$_}
-            for keys %{$regions->{北海道}->{areas}->{$area}->{areas}};
-  } elsif ($area =~ /^中川郡/) {
-      $regions->{北海道}->{areas}->{中川郡}->{areas}->{$_}
-          = $regions->{北海道}->{areas}->{$area}->{areas}->{$_}
-              for keys %{$regions->{北海道}->{areas}->{$area}->{areas}};
-  }
-}
-
 sub w ($$) {
   my ($code, $data) = @_;
   print "$code\t";
@@ -41,6 +29,7 @@ sub w ($$) {
 for (@$json) {
   my $addr = $_->[1];
   my $data = [];
+  $addr =~ s/^北海道[^郡]+郡/北海道/;
   while ($addr =~ s/^($pattern|[^都道府県市郡区町村]+[都道府県市郡区町村])//o) {
     push @$data, $1;
   }
